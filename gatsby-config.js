112dffee9cbd7d425d6d8745b3bb7cb434a3847a
babono <babono@hotmail.com>
2020-03-16 03:20:11 +0700

    update favicon + enable pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Aman | saved - insured`,
+    description: ` a technology platform, enabling companies and employees to seamlessly manage various health benefit and lifestyle programs.`,
+    author: `@Global Technologies LLC`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +18,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Aman`,
+        short_name: `Aman`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#bde5db`,
+        theme_color: `#155a50`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
